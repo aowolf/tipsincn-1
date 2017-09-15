826f853e4ec6fba55303ce9a2b77f034725acd99
Frank <31976347+aowolf@users.noreply.github.com>
2017-09-15 22:11:35 +0800

    Add files via upload

---
+++ ad0753v/inc.php
@@ -0,0 +1,5 @@
+<title>影视管理后台 - </title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="./images/woaik.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="./../favicon.ico">
---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<?php //php解密 QQ:85423296 http://uqiyi.cn/ ?>
+
+<?php 
+?><?='<div class="sidebar">
+<div class="widget widget-textasst">'; echo $aik['cebian1_ad'];?><?='</div>
+<div class="widget widget-textasst">'; echo $aik['cebian2_ad'];?><?='</div>
+
+
+<div class="widget widget-textasst">'; echo $aik['cebian4_ad'];?><?="</div>
+  
+		
+		<script>
+var ifh=$('.sidebar');
+if(ifh.height()<10){
+$('.content').css(\"width\",\"100%\");
+}else{
+}</script>";?>
